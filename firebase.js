b5798827dd3f115fa360939bea12e25b54e9de90
Carlos Andres Perez Ubeda <descarlos2013@gmail.com>
2022-12-17 10:18:06 -0600

    Maqueta de mi aplicacion

---
+++ firebase.js
@@ -0,0 +1,17 @@
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyCiBD9DAY14O9ANG_Py6615ZE64N0ECch8",
+    authDomain: "crud-notas-27730.firebaseapp.com",
+    projectId: "crud-notas-27730",
+    storageBucket: "crud-notas-27730.appspot.com",
+    messagingSenderId: "67031479605",
+    appId: "1:67031479605:web:8c4ded3a0569c667db887c"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
